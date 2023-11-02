9a4c50d03ab1c67ad758ddb4a27db42dde7c6620
krb365 <105557487+krb365@users.noreply.github.com>
2023-11-02 12:08:14 -0400

    Not working server.js

--- app/server.js
+++ app/server.js
@@ -1,1 +1,17 @@
+let express = require("express");
+let { Pool } = require("pg");
+let bcrypt = require("bcrypt");
+let env = require("../env.json");
 
+let hostname = "localhost";
+let port = 3000;
+let app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+app.listen(port, hostname, () => {
+  console.log(`http://${hostname}:${port}`);
+});
