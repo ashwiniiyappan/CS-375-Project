c568e81fc80ebdf33fa37056d121c00827532637
ashwiniiyappan <ai385@drexel.edu>
2023-11-06 23:19:19 -0500

    Updating Server.js to handle file upload

--- app/server.js
+++ app/server.js
@@ -1,5 +1,7 @@
 let express = require("express");
 let { Pool } = require("pg");
+let fileUpload = require("express-fileupload");
+let path = require("path");
 let bcrypt = require("bcrypt");
 
 // env-cmd expects .env in current directory
@@ -12,11 +14,33 @@ let hostname = "localhost";
 let port = 3000;
 let app = express();
 
+app.use(fileUpload());
+
 app.use(express.json());
 app.use(express.static("public"));
 
 let { PGUSER, PGDATA, PGPASSWORD, PGPORT, PGHOST } = process.env;
 let pool = new Pool({ PGUSER, PGDATA, PGPASSWORD, PGPORT, PGHOST });
+
+app.post("/api", (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({ message: "No files were uploaded." });
+  }
+
+  let file = req.files.file; 
+
+  let uploadPath = path.join(__dirname, "public/uploads", file.name);
+
+  file.mv(uploadPath, (err) => {
+      if (err) {
+          return res.status(500).send(err);
+      }
+
+      // save the file information to your database if needed
+
+      res.status(200).json({ message: "File uploaded successfully." });
+  });
+});
 pool.connect().then(() => {
     pool.query("SELECT * FROM content").then(result => {
         console.log(result.rows);
